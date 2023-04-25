5dc612e8924d1d7e5454b7faffc4091743076bd9
Eric Javier Hernandez Saura <2365273+ericjavier@users.noreply.github.com>
2023-04-25 12:26:27 +0200

    Simplifying APP

--- App.tsx
+++ App.tsx
@@ -3,54 +3,6 @@ import {Button, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={[styles.container]}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Details"
-        onPress={() => {
-          navigation.navigate('Details');
-        }}
-      />
-      <Button
-        title="No Header"
-        onPress={() => {
-          navigation.navigate('NoHeader');
-        }}
-      />
-      <Button
-        title="Interactive Header"
-        onPress={() => {
-          navigation.navigate('InteractiveHeader');
-        }}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={[styles.container]}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
-function NoHeaderScreen({navigation}) {
-  return (
-    <View style={[styles.container]}>
-      <Text>No Header Screen</Text>
-      <Button
-        title="Back"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
-    </View>
-  );
-}
-
 function InteractiveHeaderScreen({navigation}) {
   const [headerShown, setHeaderShown] = React.useState(true);
 
@@ -62,12 +14,6 @@ function InteractiveHeaderScreen({navigation}) {
         onPress={() => {
           navigation.setOptions({headerShown: !headerShown});
           setHeaderShown(!headerShown);
-        }}
-      />
-      <Button
-        title="Back"
-        onPress={() => {
-          navigation.goBack();
         }}
       />
     </View>
@@ -85,22 +31,7 @@ const defaultHeaderOptions = {
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{...defaultHeaderOptions}}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{...defaultHeaderOptions}}
-        />
-        <Stack.Screen
-          name="NoHeader"
-          component={NoHeaderScreen}
-          options={{...defaultHeaderOptions, headerShown: false}}
-        />
+      <Stack.Navigator initialRouteName="InteractiveHeader">
         <Stack.Screen
           name="InteractiveHeader"
           component={InteractiveHeaderScreen}
